12efb6c677f07d1bddfa3d9a9a5d256be011d7b3
Zaw Than Oo <zawthanoo.1986@gmail.com>
2025-07-18 23:22:31 +0700

    add file upload sample

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const express = require('express')
 const validator = require('validator');
 const { parse, isValid, format } = require('date-fns');
+const { setTimeout } = require('timers/promises');
 
 const app = express();
 app.use(express.json());
 
 const userList = require('./data/data.json');
-
 const port = process.env.PORT || 3132;
 
 app.get('/users', (req, res) => {
     res.json(userList);
-})
+});
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
+    await setTimeout(10000);
     for (const user of userList) {
         if (user.id == req.params.id) {
             res.status(200).json(user);
         }
     }
-})
+});
+
 
 app.get('/depusers', (req, res) => {
     let depUserList = [];
@@ -29,7 +31,7 @@ app.get('/depusers', (req, res) => {
         }
     }
     res.status(200).json(depUserList);
-})
+});
 
 app.patch('/users/update', (req, res) => {
     const updatedUser = req.body;
@@ -56,39 +58,39 @@ app.patch('/users/update', (req, res) => {
         return res.status(200).json({ message: 'User have been updated.' });
     }
     return res.status(400).json({ error: 'User not found. UserId : ' + id });
-})
+});
 
-app.post('/users/add', (req, res) => {
+app.post('/user/add', (req, res) => {
     const createdUser = req.body;
     const email = createdUser.email;
     const dob = createdUser.dob;
     const id = createdUser.id;
 
     if (!email || !validator.isEmail(email)) {
-        return res.status(400).json({ error: 'Invalid email format' });
+        return res.status(400).json({ errorCode: "E0001", message: 'Invalid email format' });
     }
 
     const parsedDate = parse(dob, "dd-MM-yyyy", new Date());
     // Check if date is valid
     if (!isValid(parsedDate)) {
-        return res.status(400).json({ error: 'Invalid date of birth format' });
+        return res.status(400).json({ errorCode: "E0002", message: 'Invalid date of birth format' });
     }
 
     // Optional: check if dob is not in the future
     if (parsedDate > new Date()) {
-        return res.status(400).json({ error: 'Date of birth cannot be in the future' });
+        return res.status(400).json({ errorCode: "E0003", message: 'Date of birth cannot be in the future' });
     }
 
     for (const user of userList) {
         if (user.id == id) {
-            return res.status(400).json({ error: 'UserId is not available' });
+            return res.status(400).json({ errorCode: "E0004", message: 'UserId is not available' });
         }
     }
     userList.push(createdUser);
     return res.status(200).json({ message: 'New user is created.' });
 
-})
+});
 
 app.listen(port, () => {
     console.log('Mock server started on:', port);
-})
+});
