cd493f970cb45c9ea29385920505efa262f801ab
zawthanoo <zawthanoo.1986@gmail.com>
2025-07-23 22:37:58 +0700

    update mock data

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const { parse, isValid, format } = require('date-fns');
 const { setTimeout } = require('timers/promises');
 const multer = require('multer');
 const path = require('path');
+const cors = require('cors');
 
 // app setup
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 
 // File Upload
@@ -27,19 +29,22 @@ const userList = require('./data/data.json');
 
 
 app.get('/users', (req, res) => {
-    res.json(userList);
+  const limit = parseInt(req.query.limit) || 10;    // default limit
+  const offset = parseInt(req.query.offset) || 0;   // default offset
+
+  const paginatedUsers = userList.slice(offset, offset + limit);
+  res.json(paginatedUsers);
 });
 
-
 app.get('/users/:id', async (req, res) => {
-    await setTimeout(10000);
     for (const user of userList) {
         if (user.id == req.params.id) {
             res.status(200).json(user);
         }
     }
-});
+    res.status(403).json({ errorCode: "E0005", message: 'User Not Found' });
 
+});
 
 app.get('/depusers', (req, res) => {
     let depUserList = [];
@@ -51,36 +56,7 @@ app.get('/depusers', (req, res) => {
     res.status(200).json(depUserList);
 });
 
-
-app.patch('/users/update', (req, res) => {
-    const updatedUser = req.body;
-    const email = updatedUser.email;
-    const dob = updatedUser.dob;
-    const id = updatedUser.id;
-
-    if (!email || !validator.isEmail(email)) {
-        return res.status(400).json({ error: 'Invalid email format' });
-    }
-
-    const parsedDate = parse(dob, "dd-MM-yyyy", new Date());
-    if (!isValid(parsedDate)) {
-        return res.status(400).json({ error: 'Invalid date of birth format' });
-    }
-
-    if (parsedDate > new Date()) {
-        return res.status(400).json({ error: 'Date of birth cannot be in the future' });
-    }
-
-    const index = userList.findIndex(user => user.id === id);
-    if (index !== -1) {
-        userList[index] = updatedUser;
-        return res.status(200).json({ message: 'User have been updated.' });
-    }
-    return res.status(400).json({ error: 'User not found. UserId : ' + id });
-});
-
-
-app.post('/user/add', (req, res) => {
+app.post('/users', (req, res) => {
     const createdUser = req.body;
     const email = createdUser.email;
     const dob = createdUser.dob;
@@ -90,10 +66,10 @@ app.post('/user/add', (req, res) => {
         return res.status(400).json({ errorCode: "E0001", message: 'Invalid email format' });
     }
 
-    const parsedDate = parse(dob, "dd-MM-yyyy", new Date());
+    const parsedDate = parse(dob, "yyyy-MM-dd", new Date());
     // Check if date is valid
     if (!isValid(parsedDate)) {
-        return res.status(400).json({ errorCode: "E0002", message: 'Invalid date of birth format' });
+        return res.status(400).json({ errorCode: "E0002", message: 'Invalid date of birth format. Use (yyyy-MM-dd)' });
     }
 
     // Optional: check if dob is not in the future
@@ -111,6 +87,43 @@ app.post('/user/add', (req, res) => {
 
 });
 
+app.patch('/users/:id', (req, res) => {
+    const updatedUser = req.body;
+    const email = updatedUser.email;
+    const dob = updatedUser.dob;
+    const id = req.params.id;
+
+    if (!email || !validator.isEmail(email)) {
+        return res.status(400).json({ error: 'Invalid email format' });
+    }
+
+    const parsedDate = parse(dob, "yyyy-MM-dd", new Date());
+    if (!isValid(parsedDate)) {
+        return res.status(400).json({ error: 'Invalid date of birth format. Use (yyyy-MM-dd)' });
+    }
+
+    if (parsedDate > new Date()) {
+        return res.status(400).json({ error: 'Date of birth cannot be in the future' });
+    }
+
+    const index = userList.findIndex(user => user.id == id);
+    if (index !== -1) {
+        updatedUser.id = id;
+        userList[index] = updatedUser;
+        return res.status(200).json({ message: 'User have been updated.' });
+    }
+    return res.status(400).json({ error: 'User not found. UserId : ' + id });
+});
+
+
+
+app.delete('/users/:id', async (req, res) => {
+    for (const user of userList) {
+        if (user.id == req.params.id) {
+            res.status(200).json(user);
+        }
+    }
+});
 
 app.post('/upload', upload.single('file')), (req, res) => {
     if(!req.file) {
@@ -126,6 +139,12 @@ app.post('/upload-multiple', upload.array('fileList')), (req, res) => {
     const fileNames = req.files.map(file => file.filename);
     res.send(`File uploaded ${fileNames.join(', ')}`);
 }
+
+// Raw APIs
+app.get('/timeout/{value}', async (req, res) => {
+    await setTimeout(req.params.value);
+    return "Success";
+});
 
 
 const port = process.env.PORT || 3132;
